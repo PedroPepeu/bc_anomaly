2db93dd0885de264340b84c9f3653079f2ae934c
PedroPepeu <pedrosteamgtav@gmail.com>
2025-11-02 09:33:11 -0300

    feat: Finished blockchain detection proceedure

--- register_anomaly.py
+++ register_anomaly.py
@@ -1,21 +1,26 @@
 from web3 import Web3
 import json
+from dotenv import load_dotenv
+import os
 
-ganache_url = "http://127.0.0.1:7545"
+load_dotenv()
+
+ganache_url = os.getenv("GANACHE_URL")
+contract_address = os.getenv("CONTRACT_ADDRESS")
 web3 = Web3(Web3.HTTPProvider(ganache_url))
 
 with open('build/contracts/AnomalyEvent.json') as f:
     contract_json = json.load(f)
-
 abi = contract_json['abi']
-
-contract_address = "0x33cdec3fE336c9Aa5918d52bBEfC1551a02CbA61"
-
 contract = web3.eth.contract(address=contract_address, abi=abi)
 
-account = web3.eth.accounts[0]
+devices = [
+    { "account": web3.eth.accounts[0], "deviceId": "device-001", "description": "DDoS detected!" },
+    { "account": web3.eth.accounts[1], "deviceId": "device-002", "description": "Malware detected!" },
+    { "account": web3.eth.accounts[2], "deviceId": "device-003", "description": "Unauthorized login attempt!" }
+]
 
-tx = contract.functions.reportAnomaly("device-xyz", "Attack detected!").transact({'from': account})
-web3.eth.wait_for_transaction_receipt(tx)
-
-print("Anomaly event registered in the blockchain")
+for d in devices:
+    tx = contract.functions.reportAnomaly(d["deviceId"], d["description"]).transact({'from': d["account"]})
+    web3.eth.wait_for_transaction_receipt(tx)
+    print(f"Event from {d['deviceId']} registered in the blockchain.")
